2f504ced7c2feaaca692ec711e1bbf75bea2ca0b
Albert Song <albb0920@gmail.com>
2012-08-02 11:40:34 +0800

    Init project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlurkFriendMoniter::Application.config.secret_token = 'c42906eb9201ea1ef33ddbc2e4b10976608c157b144978571e9da46d7b20571cf0c59ca00c8079486e448f9d3c97721536261d45b78d75e3b4fcecbbd5d45376'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PlurkFriendMoniter::Application.config.session_store :cookie_store, key: '_plurk-friend-moniter_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PlurkFriendMoniter::Application.config.session_store :active_record_store
